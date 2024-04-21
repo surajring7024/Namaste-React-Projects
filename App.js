26655d29990f4f2f4ba0be2eda391150c1f7adf1
SurajIngole <suraj.ingole7024@gmail.com>
2024-04-21 10:30:47 +0530

    episode No 2

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
 const parent = React.createElement(
   "div",
   { id: "parent" },
   [React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", { id: "heading1" }, "Im a h1 tag"),
-    React.createElement("h2", { id: "heading2" }, "Im a h1 tag")
+    React.createElement("h1", { className: "heading" }, "Im a h1 tag"),
+    React.createElement("h2", { className: "heading" }, "Im a h1 tag")
   ]),
   React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", { id: "heading3" }, "Im a h1 tag"),
-    React.createElement("h2", { id: "heading4" }, "Im a h1 tag")
+    React.createElement("h1", { className: "heading" }, "Im a h1 tag"),
+    React.createElement("h2", { className: "heading" }, "Im a h1 tag")
   ])]
 );
 
